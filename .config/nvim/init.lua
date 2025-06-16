735fc8a1ae8eaac8390602a95cd2458daf258e3c
Umut Arslan <umut_arslan@gmx.de>
2025-06-16 15:55:18 +0200

    Mon Jun 16 03:55:18 PM CEST 2025

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -1,1 +1,9 @@
-require("ide")
+-- ~/.config/nvim/init.lua
+
+-- Load Lazy.nvim setup
+require('lazy_setup')
+
+-- Load other configurations (optional)
+require('remap') -- Custom key mappings (optional)
+require('set') -- Custom key mappings (optional)
+
