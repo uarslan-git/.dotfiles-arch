6c06d7c4572f6567630b608c484c5c881a1d4b3a
Umut Arslan <umut_arslan@gmx.de>
2025-06-17 01:35:31 +0200

    Tue Jun 17 01:35:02 AM CEST 2025

--- .config/nvim/lua/plugins/copilot.lua
+++ .config/nvim/lua/plugins/copilot.lua
@@ -1,22 +1,30 @@
--- ~/.config/nvim/lua/plugins/copilot.lua
-
 return {
-  {
-    "CopilotC-Nvim/CopilotChat.nvim",
-    dependencies = {
-      { "github/copilot.vim" },
-      { "nvim-lua/plenary.nvim", branch = "master" },
-    },
-    build = "make tiktoken", -- Only on MacOS or Linux
-    opts = {
-      -- Custom options for Copilot Chat plugin
-      -- Example:
-      -- debug = true,
-    },
-    -- You can define lazy-load triggers for commands or events
-    -- Example:
-    -- event = "BufRead",
-    -- commands = { "CopilotChat" },
-  }
+    {
+        "CopilotC-Nvim/CopilotChat.nvim",
+        dependencies = {
+            { "github/copilot.vim" },
+            { "nvim-lua/plenary.nvim", branch = "master" },
+        },
+        build = "make tiktoken", -- Only on MacOS or Linux
+        opts = {
+            -- Custom options for Copilot Chat plugin
+            -- Example:
+            -- debug = true,
+        },
+        config = function(_, opts)
+            -- Custom keymap for Copilot in insert mode
+            -- This maps Ctrl-J to accept Copilot suggestion (like pressing Enter)
+            vim.g.copilot_no_tab_map = true
+            vim.api.nvim_set_keymap('i', '<C-J>', 'copilot#Accept("<CR>")', { silent = true, expr = true })
+
+            -- If you need to add more custom configurations, you can do so here
+            -- For example, enabling debug mode, if the plugin supports it:
+            -- opts.debug = true
+        end,
+        -- You can define lazy-load triggers for commands or events
+        -- Example:
+        -- event = "BufRead",
+        -- commands = { "CopilotChat" },
+    }
 }
 
